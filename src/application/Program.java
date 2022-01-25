a05b47105545aefa39ec1887c9296d221d76b126
VLV01 <vinicius.linsvasco@hotmail.com>
2022-01-25 18:29:30 -0300

    First class: Postion

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import boargame.Position;
+
 public class Program {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 	}
 }
