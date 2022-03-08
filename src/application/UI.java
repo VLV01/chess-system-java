9c2907f453ca7940782550d69a2a57607e05d590
VINICIUS LINS <vinicius.linsvasco@hotmail.com>
2022-03-08 17:22:50 -0300

    Checkmate logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -58,9 +58,16 @@ public class UI {
 		System.out.println();
 		System.out.println("Turn: " + chessMatch.getTurn());
 		// É possível mainipular as cores dos textos de saída usando "ANSI_WHITE + caractere qualquer + ANSI_RESET"
-		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
-		if (chessMatch.getCheck()) {
-			System.out.println("CHECK!");
+		if (!chessMatch.getCheckMate()) {
+			System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+			if (chessMatch.getCheck()) {
+				System.out.println("CHECK!");
+			}
+		}
+		else {
+			System.out.println("CHECKMATE!");
+			System.out.println("Winner: " + chessMatch.getCurrentPlayer());
+			
 		}
 	}
 	public static void printBoard(ChessPiece[][] pieces) {
